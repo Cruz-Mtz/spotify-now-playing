fe55c6b8a765cc904f4aa02f1b0ba4ba1c020816
Cruz <161658069+Cruz-Mtz@users.noreply.github.com>
2026-07-22 22:55:13 -0600

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,3 +1,6 @@
+const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+
 export default async function handler(req, res) {
   const { code } = req.query;
 
@@ -5,9 +8,41 @@ export default async function handler(req, res) {
     return res.status(400).send("No se recibió ningún código.");
   }
 
+  const basic = Buffer.from(
+    `${CLIENT_ID}:${CLIENT_SECRET}`
+  ).toString("base64");
+
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${basic}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      grant_type: "authorization_code",
+      code,
+      redirect_uri:
+        "https://spotify-now-playing-rho-self.vercel.app/api/callback",
+    }),
+  });
+
+  const data = await response.json();
+
+  if (data.error) {
+    return res.status(400).json(data);
+  }
+
   res.status(200).send(`
-    <h1>¡Autorización recibida! 🎉</h1>
-    <p>Código:</p>
-    <pre>${code}</pre>
+    <h1>¡Todo salió bien! 🎉</h1>
+
+    <h2>Refresh Token</h2>
+    <pre>${data.refresh_token}</pre>
+
+    <hr>
+
+    <h2>Access Token</h2>
+    <pre>${data.access_token}</pre>
+
+    <p>Guarda el Refresh Token. Lo usaremos una sola vez.</p>
   `);
 }
